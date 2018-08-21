52e9d24a76c77dc4581e8089af66950fd2b8d624
jimjunker1 <james.junker1@gmail.com>
2018-08-20 23:42:51 -0600

    need to figure out the BS dropping of characters in .csv files from Mac excel

--- abstract-test_script.R
+++ abstract-test_script.R
@@ -11,8 +11,9 @@ library(rmarkdown)
 meld <- read.csv("./TheAbstractSubmissions2018.csv")
 
 #for(i in 1:nrow(meld)){
-for(i in 1:nrow(meld[1:5,])){
+for(i in 1:nrow(meld)){
   rmarkdown::render('abstract-test_markdown.Rmd',  # file 2
-                    output_file =  paste("report_",i, '_', Sys.Date(), ".pdf", sep=''), 
+                    output_file =  paste("report_",i,'_', Sys.Date(), ".pdf", sep=''), 
                     output_dir = './reports')
 }
+droplevels(meld[1,'abstract'])
