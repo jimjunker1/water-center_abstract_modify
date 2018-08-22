0b24073053f8bafbabe40cffb2aee9375b8381a3
jimjunker1 <james.junker@msu.montana.edu>
2018-08-22 02:26:13 +0000

    almost final structure. Just need to tweak the multiple affiliation thing.

--- abstract-test_script.R
+++ abstract-test_script.R
@@ -1,4 +1,4 @@
-# file for iteratively making 
+# file for iteratively placing abstracts
 
 # File 1: Should be an R-Script 
 # contains a loop that iteratively calls an Rmarkdown file (i.e. File 2)
@@ -8,12 +8,15 @@ library(knitr)
 library(markdown)
 library(rmarkdown)
 
-meld <- read.csv("./TheAbstractSubmissions2018.csv")
+meld <- read.csv("./submission-subset_2018.csv")
 
-#for(i in 1:nrow(meld)){
+#for(i in seq_along(meld)){
+
 for(i in 1:nrow(meld)){
   rmarkdown::render('abstract-test_markdown.Rmd',  # file 2
+                    output_format = 'pdf_document',
                     output_file =  paste("report_",i,'_', Sys.Date(), ".pdf", sep=''), 
                     output_dir = './reports')
 }
-droplevels(meld[1,'abstract'])
+#droplevels(meld[1,'abstract'])
+
